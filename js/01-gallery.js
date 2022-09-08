9a75ae63335180a278e8937b6527b03b0ee09f36
Nataliia Svystula <natashasvistula@gmail.com>
2022-09-08 21:54:55 +0300

    add 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryCards = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryCards);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    }).join("");
+    
+}
+
+function onGalleryContainerClick(event) {
+    console.log(event.target)
+    if (event.target.nodeName !== "IMG") {
+      return;
+    }
+
+    const selectedImage = event.target.original;
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+    `);
+
+    instance.show();
+
+    gallery.addEventListener("keydown", e => {
+      if (e.key === "Escape") {
+        instance.close();
+      }
+    });
+}
